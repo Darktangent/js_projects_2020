78d013a55c6958f70504ef382ff17ba169ed0643
Ron Ganguly <c1ph3r9@gmail.com>
2019-12-03 17:12:29 -0600

    first commit

---
+++ basic-app_01/assets/scripts/app.js
@@ -0,0 +1,18 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult += 10;
+
+calculationDescription = `Addition with default value ${defaultResult}`;
+let errorMessage = 'An error \n occured';
+
+outputResult(currentResult, calculationDescription);
+
+// let userInput = 1;
+// let result;
+// result = 18 + userInput;
+// result = result - 3;
+// result = result * 2;
+// result = result / 2;
+// alert(result);
+// console.log(`User Input = ${userInput}`);
